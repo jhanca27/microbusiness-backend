b6aac7a158c61a41d2ee87baf40df1a37643f5fb
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-28 10:43:49 -0500

    feat: add User Entity

---
+++ src/main/java/net/quintoimpacto/ubuntuapi/entity/User.java
@@ -0,0 +1,37 @@
+package net.quintoimpacto.ubuntuapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ColumnDefault("false")
+    private boolean deleted;
+
+    private String rol;
+    private String telefono;
+
+}
