bd85392b3ebd4cd96f11d843f81e92a86e880cec
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-28 10:53:53 -0500

    feat: add User Controller

---
+++ src/main/java/net/quintoimpacto/ubuntuapi/controller/UserController.java
@@ -0,0 +1,24 @@
+package net.quintoimpacto.ubuntuapi.controller;
+
+import net.quintoimpacto.ubuntuapi.entity.User;
+import net.quintoimpacto.ubuntuapi.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<User> save(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+}
