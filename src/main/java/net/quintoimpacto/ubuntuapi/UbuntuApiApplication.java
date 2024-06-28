11b33484d79cc45cf82eee91a6f0a4f833dd0d91
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-28 09:35:20 -0500

    chore: initial spring structure

---
+++ src/main/java/net/quintoimpacto/ubuntuapi/UbuntuApiApplication.java
@@ -0,0 +1,13 @@
+package net.quintoimpacto.ubuntuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbuntuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbuntuApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/quintoimpacto/ubuntuapi/UbuntuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.quintoimpacto.ubuntuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuntuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
