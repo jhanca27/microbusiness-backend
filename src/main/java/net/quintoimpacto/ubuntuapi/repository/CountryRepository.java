d95e91402aec2e32390a67ccff215220993de781
mariacarolinagomez88 <mariacarolinagomez88@gmail.com>
2024-07-04 15:26:29 -0300

    feat:entity creation, country and province repository

---
+++ src/main/java/net/quintoimpacto/ubuntuapi/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package net.quintoimpacto.ubuntuapi.repository;
+
+import net.quintoimpacto.ubuntuapi.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
