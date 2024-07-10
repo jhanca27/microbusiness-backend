711179810a866a66576f2cdb33fc0751ab0b9238
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-07-10 13:05:37 -0500

    fix: change tget province by id to search province by name

--- src/main/java/net/quintoimpacto/ubuntuapi/service/serviceImpl/ProvinceServiceImpl.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/service/serviceImpl/ProvinceServiceImpl.java
@@ -5,22 +5,26 @@ import net.quintoimpacto.ubuntuapi.entity.Province;
 import net.quintoimpacto.ubuntuapi.mapper.ProvinceMapper;
 import net.quintoimpacto.ubuntuapi.repository.IProvinceRepository;
 import net.quintoimpacto.ubuntuapi.service.IProvinceService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class ProvinceServiceImpl implements IProvinceService {
     @Autowired
     private IProvinceRepository provinceRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
-    public List<ProvinceDTO> getAllByCountry(Long id) {
-        //List<Province> provinces = provinceRepository.getByCountryId(id);
-        List<Province> provinces = provinceRepository.findByCountryId(id);
-        //provinces.forEach(System.out::println);
-        return provinces.stream().map(ProvinceMapper::toProvinceDTO).collect(Collectors.toList());
+    public Set<ProvinceDTO> findByName(String name) {
+        ModelMapper mapper = new ModelMapper();
+        return provinceRepository.findByNameContainingIgnoreCase(name).stream()
+                .map(p -> mapper.map(p, ProvinceDTO.class))
+                .collect(Collectors.toSet());
     }
 }
