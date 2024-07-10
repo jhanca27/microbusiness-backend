1d9a1c6a7ea66599261666bec025cb79a9ddeb62
Jhan Higuera <jhiguera@qvision.com.co>
2024-07-10 10:09:56 -0500

    Ajuste micro emprendimiento -crear DTO's para registro y mostrar -cambio a update la forma en que la modifica la base de datos

--- src/main/java/net/quintoimpacto/ubuntuapi/service/IMicroBusinessService.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/service/IMicroBusinessService.java
@@ -5,13 +5,15 @@ import java.util.Optional;
 import java.util.Set;
 
 import net.quintoimpacto.ubuntuapi.dto.MicroBusinessCategoryDto;
-import net.quintoimpacto.ubuntuapi.dto.MicroBusinessDTO;
+import net.quintoimpacto.ubuntuapi.dto.microbusinessDTO.MicroBusinessDTO;
+import net.quintoimpacto.ubuntuapi.dto.microbusinessDTO.MicroBusinessRegisterDTO;
+import net.quintoimpacto.ubuntuapi.dto.microbusinessDTO.MicroBusinessShowDto;
 import net.quintoimpacto.ubuntuapi.entity.MicroBusiness;
 import net.quintoimpacto.ubuntuapi.entity.enums.Category;
 
 public interface IMicroBusinessService {
     
-    public void save(MicroBusinessDTO microBusinessDTO);
+    public MicroBusinessShowDto save(MicroBusinessRegisterDTO microBusinessDTO);
     
     public void update(MicroBusinessDTO microBusinessDTO);
 
