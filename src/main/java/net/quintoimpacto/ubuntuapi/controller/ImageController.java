49e77e5f28cb03a309d2e4836b1b7d9f81c7cc56
porta.walter <walterportadev@gmail.com>
2024-07-16 17:33:21 -0300

    agregue que se reciba un filebase64, se decodique se suba a cloudinary y resciba el string con el link del archivo

--- src/main/java/net/quintoimpacto/ubuntuapi/controller/ImageController.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/controller/ImageController.java
@@ -1,11 +1,10 @@
-
 package net.quintoimpacto.ubuntuapi.controller;
 
 import net.quintoimpacto.ubuntuapi.service.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile; // Importación faltante
 import java.util.Map;
 
 @RestController
@@ -19,6 +18,16 @@ public class ImageController {
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             Map result = cloudinaryService.uploadFile(file);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("No se pudo subir el archivo: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/uploadBase64")
+    public ResponseEntity<?> uploadBase64File(@RequestParam("fileBase64") String fileBase64) {
+        try {
+            Map result = cloudinaryService.uploadBase64File(fileBase64);
             return ResponseEntity.ok(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("No se pudo subir el archivo: " + e.getMessage());
--- src/main/java/net/quintoimpacto/ubuntuapi/service/CloudinaryService.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/service/CloudinaryService.java
@@ -1,12 +1,12 @@
 package net.quintoimpacto.ubuntuapi.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Map;
 
 @Service
@@ -17,5 +17,10 @@ public class CloudinaryService {
 
     public Map uploadFile(MultipartFile file) throws IOException {
         return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+    }
+
+    public Map uploadBase64File(String base64File) throws IOException {
+        byte[] decodedBytes = Base64.getDecoder().decode(base64File.split(",")[1]);
+        return cloudinary.uploader().upload(decodedBytes, ObjectUtils.emptyMap());
     }
 }
