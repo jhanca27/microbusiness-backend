4a13d5f1b077cfc34d29cb21f92258472ff2603f
PC <alonngadea@gmail.com>
2024-07-24 22:10:06 -0300

    Feat data users in jwt to authenticate admins

--- src/main/java/net/quintoimpacto/ubuntuapi/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/security/CustomAuthenticationSuccessHandler.java
@@ -1,23 +1,24 @@
 package net.quintoimpacto.ubuntuapi.security;
 
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Value;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.quintoimpacto.ubuntuapi.entity.User;
+import net.quintoimpacto.ubuntuapi.service.IUserService;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
 
     @Value("${SECRET_KEY}")
     private String secretKey;
@@ -25,17 +26,32 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     @Value("${FRONTEND_URL}")
     private String frontendUrl;
 
-    
+    @Autowired
+    private IUserService userService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-        String token = Jwts.builder()
-                .setSubject(oidcUser.getEmail())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hora
-                .signWith(SignatureAlgorithm.HS512, secretKey) 
-                .compact();
+        String email = oidcUser.getEmail();
 
-        response.sendRedirect(frontendUrl + "/?token=" + token); 
+        // Buscar el usuario en la base de datos
+        User user = userService.findUserByEmail(email);
+
+        if (user != null) {
+            // Crear el token JWT con los datos del usuario
+            String token = Jwts.builder()
+                    .setSubject(email)
+                    .claim("first_name", user.getFirst_name())
+                    .claim("last_name", user.getLast_name())
+                    .claim("role", user.getRole())
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hora
+                    .signWith(SignatureAlgorithm.HS512, secretKey)
+                    .compact();
+
+            response.sendRedirect(frontendUrl + "/?token=" + token);
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User not found");
+        }
     }
 }
