15a1fd295fb4ab5be8fd5e76be8b53a9b1bd2050
Alondra <alonngadea@gmail.com>
2024-08-20 13:36:57 -0300

    Refact add route in security

--- src/main/java/net/quintoimpacto/ubuntuapi/security/SecurityConfig.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/", "/login/oauth2/**", "/error").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/countries", "/provinces", "/images", "images/{id}", "/microbusiness/findAll", "/microbusiness/").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/countries", "/provinces", "/images", "images/{id}", "/microbusiness/findAll", "/microbusiness/", "microbusiness/{id}").permitAll()
                                 .requestMatchers(HttpMethod.GET,  "/questions/all", "/questions/getQuestions/{questionId}", "/answers/all", "/answers/getAnswers/{questionId}", "/publications/getAllPublications", "/publications/getAllPublications/{id}").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/contact/").permitAll()
                                 .requestMatchers("/contact/**").hasRole("ADMIN")
